2b0ddb6454ae98fa4eed587f87ca6d152a00768e
Thomas Conte <thomas@conte.com>
2017-05-12 09:34:43 +0200

    Fix REST calls and add authorization

--- explorer_1/hyperledgerpeerintf/hyperledgerpeerintf.js
+++ explorer_1/hyperledgerpeerintf/hyperledgerpeerintf.js
@@ -11,14 +11,23 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 var HyperledgerPeerIntf = function() {
-	const hyperLedgerRESTEndpoint = process.env.HYP_REST_ENDPOINT || "http://127.0.0.1:7050";
+	const hyperLedgerRESTEndpoint = process.env.HYP_REST_ENDPOINT || "http://tco7jtcwr.westus.cloudapp.azure.com";
 	var async = require('async');
 	var request = require('request');
 	this.restCall = function(uri,completion) {
 		var obj;
 		async.series( [function (callback) {
-				//console.log( ' Querying Hyperledger ' ,uri);
-				request(hyperLedgerRESTEndpoint+uri, function (error, response, body) {
+				console.log( ' Querying Hyperledger ' ,uri);
+				var options = {
+					url: hyperLedgerRESTEndpoint + uri + '?peer=peer1',
+					headers: {
+						'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0OTQ1MzAwODIsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Im9yZzEiLCJpYXQiOjE0OTQ1MjY0ODJ9.eh0DQQF9AdxKMM0mWwbaN4ERbXWgg6oz4CXzJsDfK1w',
+						'cache-control': 'no-cache',
+						'content-type': 'application/json',
+						'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0OTQ1MzAwODIsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Im9yZzEiLCJpYXQiOjE0OTQ1MjY0ODJ9.eh0DQQF9AdxKMM0mWwbaN4ERbXWgg6oz4CXzJsDfK1w'
+					}
+				};
+				request(options, function (error, response, body) {
 					if (!error && response.statusCode == 200) {
 						//console.log( ' resp ' , body);
 						if(body == null)
@@ -47,7 +56,7 @@ var HyperledgerPeerIntf = function() {
 
 
 HyperledgerPeerIntf.prototype.chain = function(callBk) {
-	this.restCall('/chain',callBk);
+	this.restCall('/channels/mychannel',callBk);
 }
 
 HyperledgerPeerIntf.prototype.peers = function(callBk) {
@@ -55,7 +64,7 @@ HyperledgerPeerIntf.prototype.peers = function(callBk) {
 }
 
 HyperledgerPeerIntf.prototype.block = function(blockNum,callBk) {
-	this.restCall('/chain/blocks/'+blockNum,callBk);
+	this.restCall('/channels/mychannel/blocks/'+blockNum,callBk);
 }
 
 
